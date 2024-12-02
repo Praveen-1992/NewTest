c2cef693accb6f47284aa68a265fd24e0689261d
Praveen-1992 <praveenmailoor@gmail.com>
2024-12-02 21:51:17 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Starting ZebraTester..."
-# Perform any initialization tasks here if needed
-java -Xmx768m ProxySniffer -JobControlTler -ExecAgent -encryptJobsOriginBackup -tz CS -execAgentHost ::1 -execAgentPort 7993 &
-exec "$@"  # Pass all arguments to the next command
+
+# Start ProxySniffer as the main process
+exec java -Xmx768m ProxySniffer -JobControlTler -ExecAgent -encryptJobsOriginBackup -tz CS -execAgentHost ::1 -execAgentPort 7993
+
+
 
 
