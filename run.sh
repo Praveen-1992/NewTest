a2a097b35d27a47faf2a104fd944d45aba175283
Praveen-1992 <praveenmailoor@gmail.com>
2024-12-02 21:46:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# Set the CLASSPATH environment variable
-export CLASSPATH=".:/home/pravi/ZebraTester/Container/xercesImpl-2.11.0.jar:/home/pravi/ZebraTester/Container/lib/*"
 echo "Running ZebraTester with arguments: $@"
 java -Xmx1024m -classpath "$CLASSPATH" Platform_Prod_US_v2 "$@"
